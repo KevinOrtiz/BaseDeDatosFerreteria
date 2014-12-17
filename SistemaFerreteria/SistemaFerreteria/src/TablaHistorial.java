b2c8d71b9be3643cc9d531e4a4ce3e97bfde91b3
root <root@kevanort@espol.edu.ec>
2014-12-17 01:43:59 -0500

    Proyecto de Base Validaciones de cajas de texto:en cuanto a entrada de datos

---
+++ SistemaFerreteria/SistemaFerreteria/src/TablaHistorial.java
@@ -0,0 +1,37 @@
+
+import javax.swing.table.AbstractTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lk
+ */
+public class TablaHistorial extends AbstractTableModel {
+    
+    private String[] columnas;
+
+    public TablaHistorial(String[] columnas) {
+        this.columnas = columnas;
+    }   
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return 0;
+    } 
+    
+}
